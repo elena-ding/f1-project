8cc9285d2c74b7274ef0e82ba6ad5a93abf0537e
elena-ding <emailelenading@gmail.com>
2025-08-29 16:12:12 -0400

    Update home.jsx
    Signed-off-by: elena-ding <emailelenading@gmail.com>

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
             >
                 <Title />
                 <motion.img 
-                    src='/assets/logos/f1_logo.png' 
+                    src='public/assets/logos/f1_logo.png' 
                     style={{width: '205px'}} 
                     animate={{opacity: [0, 1, 1], translateY: ['58px', '58px', '0px']}} 
                     transition={{duration: 2, delay: 3, ease: 'easeInOut'}}
